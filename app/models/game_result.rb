1ce41bc388ee82191034fd189a716ebf1ed44f7c
Nic <nic.miesmer@gmail.com>
2015-10-15 20:37:34 -0400

    Added devise for users, added pry

--- app/models/game_result.rb
+++ app/models/game_result.rb
@@ -1,4 +1,17 @@
 class GameResult < ActiveRecord::Base
   belongs_to :user
+  belongs_to :game
+  scope :wins, -> {where(win: true) }
+
+  def global_rank
+
+
+  end
+
+  def games_won(user)
+    
+
+  end
 
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :game_results, dependent: :destroy
+
+
+  def count_number_of_wins
+    game_results.wins.count.to_f
+  end
+
+  def count_number_of_games
+    game_results.count.to_f
+  end
+
+  def win_average
+    #binding.pry
+   (count_number_of_wins / count_number_of_games)*100
+
+  end
+
 end
